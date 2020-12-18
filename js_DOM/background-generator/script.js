55dc9cdb37ec957281df44f8afce0e08f2aa4178
Rees Dart <thedominicparker@gmail.com>
2020-12-18 15:54:29 -0800

    updated background generator with random button and minor details.

--- js_DOM/background-generator/script.js
+++ js_DOM/background-generator/script.js
@@ -2,15 +2,26 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var randomBtn = document.getElementById("randomBtn");
+
+window.onload = function setInitialGradient() {
+  css.textContent = "linear-gradient(to right, rbg(255, 0, 0), rgb(255, 255, 0));";
+}
 
 function setGradient() {
-  body.style.background = "linear-gradient(to right, " 
-  + color1.value 
-  + ", " 
-  + color2.value + ")";
+  body.style.background = "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value + ")";
 
   css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+
+randomBtn.addEventListener("click", function() {
+  color1.value = '#' + Math.floor(Math.random()*16777215).toString(16);
+  color2.value = '#' + Math.floor(Math.random()*16777215).toString(16);
+  setGradient();
+})
